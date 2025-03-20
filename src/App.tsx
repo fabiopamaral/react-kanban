a0a654d88761b885fb32385b4934dd7fc8cbe14f
Fábio Amaral <fabio.pamaral@outlook.com>
2025-03-20 20:29:37 -0300

    feat: style add Task modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import { PlusIcon } from "@radix-ui/react-icons";
-import { Box, Button, Flex, Heading } from "@radix-ui/themes";
+import { Box, Flex, Heading } from "@radix-ui/themes";
+import CreateTaskForm from "./components/CreateTaskForm";
 
 function App() {
   return (
@@ -9,9 +9,7 @@ function App() {
           <Heading size="8" weight="light">
             React Kanban
           </Heading>
-          <Button>
-            <PlusIcon /> Nova tarefa
-          </Button>
+          <CreateTaskForm />
         </Flex>
       </Box>
 
