db9055fca3292cc7b2f0452a0dfd4c29f2f06a06
Fábio Amaral <fabio.pamaral@outlook.com>
2025-03-20 19:52:54 -0300

    feat: style Header component and define Task entity

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
-import { Box, Heading } from "@radix-ui/themes";
+import { PlusIcon } from "@radix-ui/react-icons";
+import { Box, Button, Flex, Heading } from "@radix-ui/themes";
 
 function App() {
   return (
-    <Box>
-      <Heading>React Kanban</Heading>
+    <Box maxWidth="80rem" mx="auto">
+      <Box height="4rem">
+        <Flex align="center" gap="4" height="100%">
+          <Heading size="8" weight="light">
+            React Kanban
+          </Heading>
+          <Button>
+            <PlusIcon /> Nova tarefa
+          </Button>
+        </Flex>
+      </Box>
+
+      <Box>
+        <Heading as="h2">Quadro de tarefas</Heading>
+      </Box>
     </Box>
   );
 }
